3d754b7013db00960589512ff7344f462f2da3d2
yuloli <yuliia.oliichuk.s@gyarab.cz>
2026-01-23 12:15:33 +0100

    new

--- kurzy/exchange.py
+++ kurzy/exchange.py
@@ -1,6 +1,7 @@
 import httpx
 
-r = httpx.get("https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/index.html?date=15.01.2026")
-lines = r.text.splitlines()
+res = httpx.get("https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/index.html?date=22.01.2026")
 
-print(lines[0])
+print("Server answered", res.status_code)
+lines = res.text.split('\n')
+print("kurzy pro den", lines[0].split(' ')[0])  # první řádek tabulky
