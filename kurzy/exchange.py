0a913eb194fe4cebe8ec17e661bb79c78c9e5919
yuloli <yuliia.oliichuk.s@gyarab.cz>
2026-02-06 09:36:13 +0100

    update colorama

--- kurzy/exchange.py
+++ kurzy/exchange.py
@@ -1,11 +1,15 @@
 import httpx
+from colorama import Fore, Style, Back, just_fix_windows_console
 # from datetime import datetime 
 
 # current_date = datetime.now().strftime('%d.%m.%Y')
+just_fix_windows_console() 
 
 res = httpx.get('https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt')
 
-print("server odpovedel:", res.status_code)
+print(Style.DIM + "server odpovedel:", res.status_code)
+print(Style.RESET_ALL)
+
 lines = res.text.split('\n')
 print("Kruzy pro den:", lines[0].split(" ")[0])
 
@@ -26,16 +30,21 @@ print("1) EUR -> CZK")
 print("2) CZK -> EUR")
 print() 
 mena_choice = input("vyber bud 1 nebo 2: ") 
-
+ 
 if mena_choice == "1": 
     value_in = float(input("Kolik mas eur? "))
     value_out = value_in * rate
+    print(Back.RED)
     print(f"Tak to je {value_out:.2f} korun.")
 
 elif mena_choice == "2": 
     value_in = float(input("Kolik mas korun? "))
     value_out = value_in / rate
+    print(Back.RED)
     print(f"Tak to je {value_out:.2f} eur.")
 
 else: 
+    print(Fore.RED) 
     print("neplatny vstup")
+    
+print(Style.RESET_ALL)
