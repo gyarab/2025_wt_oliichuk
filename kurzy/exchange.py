718901368e8d98d53f6476ed28e6ab9570ef7fa3
yuloli <yuliia.oliichuk.s@gyarab.cz>
2026-02-06 09:16:33 +0100

    homework

--- kurzy/exchange.py
+++ kurzy/exchange.py
@@ -1,7 +1,41 @@
 import httpx
+# from datetime import datetime 
 
-res = httpx.get("https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/index.html?date=22.01.2026")
+# current_date = datetime.now().strftime('%d.%m.%Y')
 
-print("Server answered", res.status_code)
+res = httpx.get('https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt')
+
+print("server odpovedel:", res.status_code)
 lines = res.text.split('\n')
-print("kurzy pro den", lines[0].split(' ')[0])  # první řádek tabulky
+print("Kruzy pro den:", lines[0].split(" ")[0])
+
+line_euro = ""
+for line in lines:
+    if "EUR" in line:
+        line_euro = line
+        break
+
+rate_str = line_euro.split('|')[-1].replace(',' , '.')
+rate = float(rate_str)
+
+print("Kurz eura je", rate, "Kc")
+
+print("Jaky chces prevod? ")
+print() 
+print("1) EUR -> CZK") 
+print("2) CZK -> EUR")
+print() 
+mena_choice = input("vyber bud 1 nebo 2: ") 
+
+if mena_choice == "1": 
+    value_in = float(input("Kolik mas eur? "))
+    value_out = value_in * rate
+    print(f"Tak to je {value_out:.2f} korun.")
+
+elif mena_choice == "2": 
+    value_in = float(input("Kolik mas korun? "))
+    value_out = value_in / rate
+    print(f"Tak to je {value_out:.2f} eur.")
+
+else: 
+    print("neplatny vstup")
